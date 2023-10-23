54f2d878d941ada9264611637df29c0efbffdcf6
Ruthvik283 <ruthvik92931@gmail.com>
2023-10-23 19:44:10 +0530

    Added new carousel

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -4,6 +4,7 @@ import Navbar from './Navbar';
 import OffCanvasNavbar from './OffCanvasNavbar';
 import Carousel from './Carousel';
 import Card from './Card';
+import CarouselControlsInside from './Carousel2';
 import { BsFillArrowRightCircleFill, BsFillArrowLeftCircleFill } from 'react-icons/bs'
 
 export default function Dashboard() {
@@ -25,14 +26,8 @@ export default function Dashboard() {
 
                 <div className='border-0 text-white text-3xl font-weight mt-32 mx-[10%]'>Discover Cricket Events</div>
                 
-                <div className='flex justify-center border-0 mt-20'>
-                    <BsFillArrowLeftCircleFill className='switch m-auto h-10 ml-[5%]'/>
-                    <div className='grid grid-cols-3 gap-10 mx-[10%] border-0'>
-                        <Card />
-                        <Card />
-                        <Card />
-                    </div>
-                    <BsFillArrowRightCircleFill className='switch m-auto h-10 mr-[5%]'/>
+                <div className='ml-20  mr-20 border-0 mt-20'>
+                    <CarouselControlsInside />
                 </div>
                 
                 <div className='border-0 text-white text-3xl font-weight mt-32 mx-[10%]'>Discover Football Events</div>
