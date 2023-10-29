2902006e973ac43033da22751d9716c8aed5c4a2
Naren Sai <narenkumarsai2005@gmail.com>
2023-10-29 15:33:00 +0530

    Fixed password hashing

--- backend/stadium/urls.py
+++ backend/stadium/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from . import views
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
+from .views import * 
+from .serializers import * 
 
 urlpatterns = [
-    path('dashboard/', views.dashboard, name='dashboard'),
+    path('token/',TokenObtainPairView.as_view(), name = 'token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name = 'token_refresh'),
+    path('register/', RegisterAPI.as_view(), name = 'register'),
+    # path('dashboard/', views.dashboard, name='dashboard'),
     # path('events/', views.EventList.as_view, name='events-list')
-    path('events/', views.getEvents, name='get_events')
+    # path('events/', views.getEvents, name='get_events')
 ]
--- backend/stadium/views.py
+++ backend/stadium/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from rest_framework import serializers
+from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import *
 from .serializers import *
+
+class RegisterAPI(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 def dashboard(request):
     return HttpResponse("Hi stadium")
