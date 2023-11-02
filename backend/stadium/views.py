e8adf42dbe23ba0c9a292ee13b540c347aee16f6
Naren Sai <narenkumarsai2005@gmail.com>
2023-11-02 20:49:57 +0530

    updated models, APIs and booking page

--- backend/stadium/views.py
+++ backend/stadium/views.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponse, Http404
+# from django.http import HttpResponse, Http404
+from rest_framework.exceptions import NotFound
 from django.db.models import Q
 from rest_framework.views import APIView
 from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView
@@ -30,42 +31,20 @@ class showEvents(ListAPIView):
 
 class getEventById(APIView):
     def get(self,request,id):
-        stadium_id = Event.objects.get(event_id = id).stadium_id
+        try:
+            stadium_id = Event.objects.get(event_id = id).stadium_id
+        except:
+            raise NotFound("Event not found")
         event_data = EventSerializer(Event.objects.get(event_id = id)).data
-        event_data['stadium'] = StadiumSerializer(Stadium.objects.get(stadium_id=stadium_id)).data
-        event_data['prices'] = SectorSerializer(Sector.objects.filter(stadium = stadium_id), many = True).data  
+        event_data['prices'] = SectorPriceSerializer(SectorPrice.objects.filter(event_id = id), many = True).data  
         return Response(event_data)
-        
 
-# class getStadiumById(RetrieveAPIView):
-#     queryset = Stadium.objects.all() 
-#     serializer_class = StadiumSerializer
-# @api_view(['GET'])
-# def getStadiumById(request, id):
-#     sid = Stadium.objects.get(stadium_id=id)
-#     serializer = StadiumSerializer(sid)
-#     return Response(serializer.data)  
-class getStadiumById(APIView):
-    def get(self,request,id):
-        stadium = Stadium.objects.get(stadium_id=id)
-        serializer = StadiumSerializer(stadium) 
-        sectors = Sector.objects.filter(stadium = id)
-        sectorserializer = SectorSerializer(sectors, many = True)
-        data = serializer.data 
-        data['sectors'] = sectorserializer.data
-        return Response(data)
-    
-
-class getEvents(ListAPIView):
-    serializer_class = EventSerializer
-    def get_queryset(self):
-        query = self.request.GET.get('query', '')
+class getEvents(APIView):
+    def get(request, self):
         date = timezone.now()
         all_events = Event.objects.all()
         if date:
             all_events = all_events.filter(date_time__gte=date)
-
         events_data = EventSerializer(all_events, many=True).data
         return Response(events_data)
-
 
--- frontend/src/pages/BookingPage.js
+++ frontend/src/pages/BookingPage.js
@@ -1,49 +1,110 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import Counter from '../utils/Counter';
-import axios from 'axios'
-import toast from 'react-hot-toast'
-import EventContext from '../context/EventContext'
-
+import React, { useContext, useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import Counter from "../utils/Counter";
+import OffCanvasNavbar from "../components/OffCanvasNavbar";
+import Navbar from "../components/Navbar";
+import axios from "axios";
+import toast from "react-hot-toast";
+import EventContext from "../context/EventContext";
 
 const BookingPage = () => {
-    const {event_id} = useParams();  // getting event id from url
+const { event_id } = useParams();
+const contextData = useContext(EventContext);
+let { setEventdata } = useContext(EventContext);
 
-    return (
-        <>
-            
-            <div className='w-full bg-gradient-to-r from-slate-950 to-slate-700'>
-                <OffCanvasNavbar />
-                <Navbar />
-                <Link to={ `/event/${event_id}` } >
-                    <div className='text-teal-500'>&#60;Back to event page</div>
-                </Link>
-                <ul className='text-gray-100'>
-                    {
-                        event.prices.map((sector) => (
-                            <li key={sector.sector_id}>
-                                {sector.sector_name} - {sector.sector_price}
-                                <input 
-                                    type="number"
-                                    min="0"
-                                    onChange={(e)=>handleQuantityChange(sector.sector_id,parseInt(e.target.value, 10))}
-                                    className='bg-inherit border-red-600'
-                                />
-                            </li>
-                        ))
-                    }
-                </ul>   
-                <div className='text-gray-300'>
-                    { totalPrice }
-                </div>
+const [event, setEvent] = useState({
+    event_id: 0,
+    event_name: "",
+    date_time: "",
+    event_description: "",
+    stadium: {
+    stadium_id: 0,
+    stadium_name: "",
+    location: "",
+    coordinates: "",
+    city: "",
+    },
+    prices: [],
+});
 
-                <footer className="bg-slate-800 text-white text-center py-10">
-                    Contact us <br />
-                    a | a | a | a
-                </footer>
-            </div>
-        </>
-    )
-}
+useEffect(() => {
+    if (!(event_id == contextData.event_data.event_id)) {
+    axios
+        .get(`/api/get_event/${event_id}`)
+        .then((res) => {
+        setEventdata(res.data);
+        setEvent(res.data);
+        })
+        .catch((err) => {
+        toast.fail("404 event does not exist");
+        });
+    } else {
+    setEvent(contextData.event_data);
+    }
+}, []);
 
-export default BookingPage
+useEffect(() => {
+    document.title = event.event_name + " - Sports League";
+}, [event.event_name]);
+
+const [quantities, setQuantities] = useState({});
+const [totalPrice, setTotalPrice] = useState(0);
+const handleQuantityChange = (sector_id, quantity) => {
+    setQuantities((prevQuantities) => ({
+    ...prevQuantities,
+    [sector_id]: quantity,
+    }));
+};
+useEffect(() => {
+    let totalCost = 0;
+    for (const sector_id in quantities) {
+    const quantity = quantities[sector_id];
+    const sector = event["prices"].find(
+        (sector) => sector.sector_id == sector_id
+    );
+    if (sector) {
+        totalCost += sector.event_price * quantity;
+    }
+    }
+    setTotalPrice(totalCost);
+}, [quantities]);
+
+return (
+    <>
+    <div className="w-full bg-gradient-to-r from-slate-950 to-slate-700">
+        <OffCanvasNavbar />
+        <Navbar />
+        <Link to={`/event/${event_id}`}>
+        <div className="text-teal-500">&#60;Back to event page</div>
+        </Link>
+        <ul className="text-gray-100">
+        {event.prices.map((sector) => (
+            <li key={sector.sector_id}>
+            {sector.sector_name} - {sector.event_price} -
+            <input
+                type="number"
+                min="0"
+                onChange={(e) =>
+                handleQuantityChange(
+                    sector.sector_id,
+                    e.target.value
+                    ? parseInt(e.target.value, 10)
+                    : parseInt(0, 10)
+                )
+                }
+                className="bg-inherit border-red-600"
+            />
+            </li>
+        ))}
+        </ul>
+        <div className="text-gray-300">{totalPrice}</div>
+
+        <footer className="bg-slate-800 text-white text-center py-10">
+        Contact us <br />a | a | a | a
+        </footer>
+    </div>
+    </>
+);
+};
+
+export default BookingPage;
